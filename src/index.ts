b6535d32c4d66f0746fd3049310fa30cc5881306
Juan Feris <juan.feris@gorillalogic.com>
2021-03-25 17:18:46 -0500

    Removing Deprecated

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+
 import { router } from './routes/loginRoutes'
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
 app.use(router)
 
 app.listen(3000, () => {
