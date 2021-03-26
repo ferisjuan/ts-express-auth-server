35f6648123766c7f6dcf688b9f8f5a78c271b149
Juan Feris <juan.feris@gorillalogic.com>
2021-03-26 15:16:47 -0500

    Login and Logout

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cookieSession from 'cookie-session'
 
 import { router } from './routes/loginRoutes'
 
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieSession({ keys: ['some string'] }))
 app.use(router)
 
 app.listen(3000, () => {
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -25,10 +25,37 @@ router.get('/login', (_req: Request, res: Response) => {
 router.post('/login', (req: RequestWithBody, res: Response) => {
 	const { email, password } = req.body
 
-	if (email) {
-		return res.send(email.toUpperCase())
+	if (email && password && email === 'hi@hi.com' && password === 'password') {
+		req.session = { loggedIn: true }
+		res.redirect('/')
+	} else {
+		res.send('Invalid email or password')
 	}
-	res.send('You must provide an email')
+})
+
+router.get('/', (req: Request, res: Response) => {
+	if (req.session && req.session.loggedIn) {
+		res.send(`
+    <div>
+      <div>You are logged in
+      <a href="logout">Logout</a>
+      </div>
+    </div>
+    `)
+	} else {
+		res.send(`
+    <div>
+      <div>You are not logged in
+      <a href="login">Login</a>
+      </div>
+    </div>
+    `)
+	}
+})
+
+router.get('/logout', (req: Request, res: Response) => {
+	req.session = undefined
+	res.redirect('/')
 })
 
 export { router }
