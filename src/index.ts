9057ee2850ea7a6b3f14abebe387ea173ecdbe03
Juan Feris <juan.feris@gorillalogic.com>
2021-04-14 17:54:21 -0500

    Adding Singleton for the Router

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import cookieSession from 'cookie-session'
 
 import { router } from './routes/loginRoutes'
-import { router as controllerRouter } from './controllers/decorators/controller'
+import { AppRouter } from './AppRouter'
 
 import './controllers/LoginController'
 
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieSession({ keys: ['some string'] }))
 app.use(router)
-app.use(controllerRouter)
+app.use(AppRouter.getInstance())
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000')
