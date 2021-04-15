85c3bf6ca6ede25c071f0e0cf636d2cadfcdb097
Juan Feris <juan.feris@gorillalogic.com>
2021-04-15 08:46:00 -0500

    Better Logging

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
 
 export function logger(req: Request, res: Response, next: NextFunction) {
-	console.log('Request was made!!')
+	console.log('Request was made:', req.method, req.url)
 	next()
 }
